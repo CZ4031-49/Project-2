63de728a3dab266304d27ba565dd77db1910472b
H0xC0000005 <74247664+H0xC0000005@users.noreply.github.com>
2022-11-13 20:51:47 +0800

    merged everything to annotator.py

--- annotator.py
+++ annotator.py
@@ -1,8 +1,183 @@
 from __future__ import annotations
 
-from typing import Any
+import json
+import re
 
-from utilities_and_parsing import *
+
+# from utilities_and_parsing import *
+import sqlparse
+
+
+class CONSTANTS:
+    LOG_FILE_FULL_PLAN_PREFIX = "Plans output:"
+
+    OUTER_PLAN_NAME = "Plan"
+    INTERMEDIATE_PLAN_NAME = "Plans"
+    NODE_TYPE_NAME = "Node Type"
+    PLAN_ROW_SIZE_NAME = "Plan Rows"
+    PLAN_WIDTH_SIZE_NAME = "Plan Width"
+    PARALLEL_AWARE_NAME = "Parallel Aware"
+    ASYNC_CAPABILITY_NAME = "Async Capable"
+
+    RELATION_NAME = "Relation Name"
+    START_COST_NAME = "Startup Cost"
+    TOTAL_COST_NAME = "Total Cost"
+
+    ALIAS_NAME = "Alias"
+
+    node_type_mapping_to_sql_keyword = {"Limit": "LIMIT"}
+
+    # join constants
+    JOIN_TYPE_NAME = "Join Type"
+    JOIN_FILTER_NAME = "Join Filter"
+
+    # scan constants
+    FILTER_NAME = "Filter"
+    INDEX_COND_NAME = "Index Cond"
+    INDEX_NAME = "Index Name"
+    PARENT_RELATIONSHIP_NAME = "Parent Relationship"
+    SCAN_DIRECTION_NAME = "Scan Direction"
+
+    BITMAPHEAPSCAN_CONDITION_NAME = "Recheck Cond"
+    INDEX_CONDITION_NAME = "Index Cond"
+
+    # operation type
+    SCAN = "SCAN"
+    JOIN = "JOIN"
+    EMIT = "EMIT"
+    AUX = "AUXILIARY"
+
+    # sub operation types
+    # SCAN
+    INDEXSCAN = "Index Scan"
+    SEQSCAN = "Sequential Scan"
+    INDEXONLYSCAN = "Index Only Scan"
+    BITMAPSCAN = "Bitmap Scan"
+    BITMAPINDEXSCAN = "Bitmap Index Scan"
+    BITMAPHEAPSCAN = "Bitmap Heap Scan"
+    TIDSCAN = "TID Scan"
+
+    SCANTYPES = (INDEXSCAN, SEQSCAN, INDEXONLYSCAN, BITMAPSCAN, TIDSCAN, BITMAPHEAPSCAN, BITMAPINDEXSCAN)
+
+    # JOIN
+    HASHJOIN = "Hash Join"
+    INDEXJOIN = "Index Join"
+    NL = "Nested Loop"
+    SORTMERGEJOIN = "Sort Merge Join"
+
+    JOINTYPES = (HASHJOIN, INDEXJOIN, NL, SORTMERGEJOIN)
+
+    # EMIT
+    MATERIALIZE = "Materialize"
+
+    EMITTYPES = (MATERIALIZE)
+
+    # AUX
+    LIMIT = "Limit"
+    SORT = "Sort"
+    AGGREGATE = "Aggregate"
+    HASHAGGREGATE = "Hash Aggregate"
+    GROUPBYAGGREGATE = "Group By Aggregate"
+    UNIQUE = "Unique"
+    LOCKROWS = "LockRows"
+    SETOP = "SetOp"
+    MEMOIZE = "Memoize"
+
+    APPEND = "Append"
+    SUBQUERYSCAN = "Subquery Scan"
+
+    AUXTYPES = (LIMIT, SORT, AGGREGATE, HASHAGGREGATE, GROUPBYAGGREGATE, UNIQUE, LOCKROWS, SETOP, MEMOIZE)
+
+    # attribute constant for explanation insertion
+    EXPLANATION = "Explanation"
+
+    # status used by annotation to denote few node status
+    HASCHILDWITHINDEX = "Has Child With Index"
+    CONTAINRELATION = "Contain Relation"
+
+
+def read_log_file(file_name):
+    parsed_log_lines = []
+    with open(file_name, "r") as log_file:
+        for line in log_file.readlines():
+            full_pattern_regex = f"DEBUG:root:{CONSTANTS.LOG_FILE_FULL_PLAN_PREFIX} "
+            if re.search(full_pattern_regex, line):
+                line = line[len(full_pattern_regex):]
+                parsed_log_lines.append(line)
+    return parsed_log_lines
+
+
+def chop_plan_dict(plan_in: dict,
+                   what_to_remain: list | tuple = (CONSTANTS.NODE_TYPE_NAME,
+                                                   CONSTANTS.INTERMEDIATE_PLAN_NAME,
+                                                   CONSTANTS.OUTER_PLAN_NAME,
+                                                   CONSTANTS.RELATION_NAME,
+                                                   CONSTANTS.FILTER_NAME,
+                                                   CONSTANTS.JOIN_FILTER_NAME,
+                                                   )):
+    """
+    view a summary based on selected attributes, and this function does an easy job inplace. remember
+    to copy your dict and since this is inplace to visualize should still dump as json or sth else
+    :param plan_in: plan dict to summarize
+    :param what_to_remain: what attribute to summarize
+    :return: None
+    """
+
+    def chop_keys(x):
+        list_of_keys = tuple(cur_plan.keys())
+        for key in list_of_keys:
+            if key not in what_to_remain:
+                x.pop(key)
+
+    frontier = [plan_in]
+    while len(frontier) > 0:
+        cur_plan = frontier.pop()
+        try:
+            plans = cur_plan[CONSTANTS.INTERMEDIATE_PLAN_NAME]
+            chop_keys(cur_plan)
+            if isinstance(plans, (list, tuple)):
+                for next_plan in plans:
+                    # should be a _iter of dicts
+                    frontier.append(next_plan)
+            else:
+                frontier.append(plans)
+        except KeyError:
+            # first try is this a root relation
+            try:
+                root_plan = cur_plan[CONSTANTS.OUTER_PLAN_NAME]
+                # if sucessful here, should be a root
+                chop_keys(cur_plan)
+                frontier.append(root_plan)
+            except KeyError:
+                chop_keys(cur_plan)
+                print(f"there's neither {CONSTANTS.OUTER_PLAN_NAME} nor {CONSTANTS.INTERMEDIATE_PLAN_NAME} "
+                      f"attribute in this plan, it's either a leaf or error.")
+
+
+def parse_sql_query_with_categories(query: str) -> dict[str, list]:
+    parsed = sqlparse.parse(query)[0]
+
+    tokens = {"all": [], "identifiers": [], }
+    for idx, elem in enumerate(parsed.tokens):
+        elem: sqlparse.sql.Token | sqlparse.sql.Identifier | sqlparse.sql.Comparison
+        if isinstance(elem, sqlparse.sql.Identifier):
+            tokens["identifiers"].append(elem)
+        tokens["all"].append(elem)
+
+    return tokens
+
+
+def map_node_type_to_operation(node_type: str) -> str:
+    if node_type in CONSTANTS.SCANTYPES:
+        return CONSTANTS.SCAN
+    elif node_type in CONSTANTS.JOINTYPES:
+        return CONSTANTS.JOIN
+    elif node_type in CONSTANTS.EMITTYPES:
+        return CONSTANTS.EMIT
+    elif node_type in CONSTANTS.AUXTYPES:
+        return CONSTANTS.AUX
+    else:
+        raise ValueError(f"unidentified node type: {node_type}")
 
 
 def annotate_query_plan(plan: dict) -> None:
@@ -37,7 +212,7 @@ def annotate_query_plan(plan: dict) -> None:
 
 
 def _explain_node(plan: dict, node_type: str) -> None:
-    explanation = f"Node type: {plan[CONSTANTS.NODE_TYPE_NAME]}: \n" 
+    explanation = f"Node type: {plan[CONSTANTS.NODE_TYPE_NAME]}: \n"
     # if plan[CONSTANTS.NODE_TYPE_NAME] == "Limit":
     #     print("debug")
     #     print(node_type)
@@ -302,35 +477,35 @@ sample_query = """ SELECT s_address, p_partkey
                 """
 
 
-def annotate_plan(plan_dict: dict[str, Any], use_plan_log=False, log_file_name=None):
-    if use_plan_log and log_file_name:
-        raw_plans = read_log_file("test_prep.log")
-        first_complete_plan_str = raw_plans[0]
-        print(first_complete_plan_str)
-        first_complete_plan = ast.literal_eval(first_complete_plan_str)
-    else:
-        first_complete_plan = plan_dict
-
-    json_dumped_str = json.dumps(first_complete_plan, sort_keys=True, indent=4)
-    print(json_dumped_str)
-
-    p = first_complete_plan.copy()
-    print(p)
-
-    """
-    sample query:
-    "SELECT * FROM customer c JOIN orders o ON o.o_custkey=c.c_custkey WHERE c.c_custkey < 100 LIMIT 10"
-    SELECT *
-    FROM customer c, orders o
-    WHERE o.o_custkey=c.c_custkey AND
-        c.c_custkey < 100 AND
-        o.o_custkey > 50 OR
-        o.0_custkey = 1
-
-    """
-
-    sample_tokens = parse_sql_query_with_categories(sample_query)
-
-    annotate_query_plan(p)
-    json_dumped_str = json.dumps(p, sort_keys=True, indent=4)
-    print(json_dumped_str)
+# def annotate_plan(plan_dict: dict[str, Any], use_plan_log=False, log_file_name=None):
+#     if use_plan_log and log_file_name:
+#         raw_plans = read_log_file("test_prep.log")
+#         first_complete_plan_str = raw_plans[0]
+#         print(first_complete_plan_str)
+#         first_complete_plan = ast.literal_eval(first_complete_plan_str)
+#     else:
+#         first_complete_plan = plan_dict
+#
+#     json_dumped_str = json.dumps(first_complete_plan, sort_keys=True, indent=4)
+#     print(json_dumped_str)
+#
+#     p = first_complete_plan.copy()
+#     print(p)
+#
+#     """
+#     sample query:
+#     "SELECT * FROM customer c JOIN orders o ON o.o_custkey=c.c_custkey WHERE c.c_custkey < 100 LIMIT 10"
+#     SELECT *
+#     FROM customer c, orders o
+#     WHERE o.o_custkey=c.c_custkey AND
+#         c.c_custkey < 100 AND
+#         o.o_custkey > 50 OR
+#         o.0_custkey = 1
+#
+#     """
+#
+#     sample_tokens = parse_sql_query_with_categories(sample_query)
+#
+#     annotate_query_plan(p)
+#     json_dumped_str = json.dumps(p, sort_keys=True, indent=4)
+#     print(json_dumped_str)
